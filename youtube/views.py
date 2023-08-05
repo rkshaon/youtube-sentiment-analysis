e3f2b40e780c91a809814b95c10ab49efe7f79bc
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-08-06 01:33:53 +0600

    youtube - get link and return video details

--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,8 @@
 from django.template import loader
 from django.http import HttpResponse
 
+from pytube import YouTube
+
 from youtube.forms import LinkForm
 
 
@@ -8,11 +10,18 @@ from youtube.forms import LinkForm
 def index(request):
     form = LinkForm()
 
+    data = {}
+
     if request.method == 'POST':
-        print(f"\nLink: {request.POST['link']}\n")
+        video = YouTube(url=request.POST['link'])
+        data['title'] = video.title
+        data['length'] = video.length
+        data['author'] = video.author
+        data['length'] = f"{ data['length'] // 60 }:{ data['length'] % 60}"
 
     context = {
         'form': form,
+        'data': data,
     }
     template = loader.get_template('index.html')
 
